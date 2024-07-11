fb75966e033760c5d1a8d18c8382639ba81dbf21
Alex Partin <25892264+adpartin@users.noreply.github.com>
2024-07-11 10:26:16 -0700

    hpo_subprocess

--- hpo_subprocess.py
+++ hpo_subprocess.py
@@ -7,28 +7,36 @@ and the env vars $IMPROVE_DATA_DIR and $PYTHONPATH are set:
 export IMPROVE_DATA_DIR="./csa_data/"
 export PYTHONPATH=$PYTHONPATH:/path/to/IMPROVE_lib
 
-mpirun -np 2 python hpo_subprocess.py
+It also assumes that your processed training data is at: "ml_data/{source}-{source}/split_{split}"
+validation data is at: "ml_data/{source}-{source}/split_{split}"
+model output files will be saved at "dh_hpo_improve/{source}/split_{split}"
 
-TODO: how to distribute HPO to mulitple GPUs?
+mpirun -np 5 python hpo_subprocess.py
 """
 # import copy
 import json
 import subprocess
-import os
 import pandas as pd
+import os
+import logging
+import mpi4py
 
 from deephyper.evaluator import Evaluator, profile
 from deephyper.evaluator.callback import TqdmCallback
 from deephyper.problem import HpProblem
 from deephyper.search.hps import CBO
 
-import mpi4py
+from mpi4py import MPI
+import socket
+
+
+# --------------------------
+# Enable using multiple GPUs
+# --------------------------
 mpi4py.rc.initialize = False
 mpi4py.rc.threads = True
 mpi4py.rc.thread_level = "multiple"
 mpi4py.rc.recv_mprobe = False
-
-from mpi4py import MPI
 
 if not MPI.Is_initialized():
     MPI.Init_thread()
@@ -36,12 +44,30 @@ if not MPI.Is_initialized():
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
+# local_rank = os.environ["PMI_LOCAL_RANK"]  # TODO. error here??
 
-# breakpoint()
 num_gpus_per_node = 4
 gpu_id0 = 4
 os.environ["CUDA_VISIBLE_DEVICES"] = str(rank % num_gpus_per_node + gpu_id0)
-# os.environ["CUDA_VISIBLE_DEVICES"] = "6,7"
+
+# Leo's code
+# CUDA_VISIBLE_DEVICES is now set via set_affinity_gpu_polaris.sh
+# uncomment the below commands if running via interactive node
+#num_gpus_per_node = 4
+#os.environ["CUDA_VISIBLE_DEVICES"] = str(rank % num_gpus_per_node)
+#cuda_name = "cuda:" + str(rank % num_gpus_per_node)
+
+
+# ---------------------
+# Enable logging
+# ---------------------
+logging.basicConfig(
+    # filename=f"deephyper.{rank}.log, # optional if we want to store the logs to disk
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(filename)s:%(funcName)s - %(message)s",
+    force=True,
+)
+
 
 # ---------------------
 # Hyperparameters
@@ -54,19 +80,22 @@ problem.add_hyperparameter((1e-6, 1e-2, "log-uniform"),
 # problem.add_hyperparameter((0, 0.5), "dropout", default_value=0.0)
 # problem.add_hyperparameter([True, False], "early_stopping", default_value=False)
 
+
 # ---------------------
 # Some IMPROVE settings
 # ---------------------
 # source = "CCLE"
-source = "CTRPv2"
-# source = "gCSI"
+# source = "CTRPv2"
+source = "gCSI"
 # source = "GDSCv1"
 # source = "GDSCv2"
 split = 0
 train_ml_data_dir = f"ml_data/{source}-{source}/split_{split}"
 val_ml_data_dir = f"ml_data/{source}-{source}/split_{split}"
-model_outdir = f"{source}_dh_hpo_improve/{source}/split_{split}"
-log_dir = f"{source}_dh_hpo_logs/"
+#model_outdir = f"{source}_dh_hpo_improve/{source}/split_{split}"
+model_outdir = f"dh_hpo_improve/{source}/split_{split}"  # Leo
+#log_dir = f"{source}_dh_hpo_logs/"
+log_dir = "dh_hpo_logs/"  # Leo
 subprocess_bashscript = "subprocess_train.sh"
 
 
@@ -84,6 +113,8 @@ def run(job, optuna_trial=None):
     #     params.update(config)
 
     model_outdir_job_id = model_outdir + f"/{job.id}"
+    learning_rate = job.parameters["learning_rate"]
+    batch_size = job.parameters["batch_size"]
 
     # val_scores = main_train_grapdrp([
     #     "--train_ml_data_dir", str(train_ml_data_dir),
@@ -94,14 +125,17 @@ def run(job, optuna_trial=None):
     subprocess_res = subprocess.run(
         [
             "bash", subprocess_bashscript,
-             str(train_ml_data_dir),
-             str(val_ml_data_dir),
-             str(model_outdir_job_id)
+            str(train_ml_data_dir),
+            str(val_ml_data_dir),
+            str(model_outdir_job_id),
+            str(learning_rate),
+            str(batch_size),
+            str(os.environ["CUDA_VISIBLE_DEVICES"])
         ], 
         capture_output=True, text=True, check=True
     )
     
-    # print(subprocess_res.stdout)
+    print(subprocess_res.stdout)
     # print(subprocess_res.stderr)
 
     # Load val_scores and get val_loss
@@ -120,8 +154,10 @@ def run(job, optuna_trial=None):
 
 
 if __name__ == "__main__":
+
     import time
     t0 = time.time()
+
     with Evaluator.create(
         run, method="mpicomm", method_kwargs={"callbacks": [TqdmCallback()]}
     ) as evaluator:
@@ -138,12 +174,18 @@ if __name__ == "__main__":
 
             # max_evals = 2
             # max_evals = 4
-            # max_evals = 10
+            max_evals = 10
             # max_evals = 20
-            max_evals = 50
+            # max_evals = 50
             # max_evals = 100
+
             results = search.search(max_evals=max_evals)
             results = results.sort_values("m:val_loss", ascending=True)
             results.to_csv(model_outdir + "/hpo_results.csv", index=False)
+
+    # print("current node: ", socket.gethostname(),
+    #       "; current rank: ", rank,
+    #       "; local rank", local_rank,
+    #       "; CUDA_VISIBLE_DEVICE is set to: ", os.environ["CUDA_VISIBLE_DEVICES"])
 
     print(f"Finished deephyper HPO (rank {rank}) in {(time.time() - t0) / 60} mins.")
