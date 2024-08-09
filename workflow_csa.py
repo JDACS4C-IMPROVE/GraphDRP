8c61db8728d04c9ff68a7d9712b78ec9eefb9752
priyanka9991 <pvasanthakumari@anl.gov>
2024-08-09 14:34:03 -0500

    debug preprocess

--- workflow_csa.py
+++ workflow_csa.py
@@ -68,7 +68,7 @@ config_lambda = Config(
 )
 
 parsl.clear()
-parsl.load()
+parsl.load(config_lambda)
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 fdir = Path(__file__).resolve().parent
