7da69d0888147b31ddeb12056d94dbb69c8b99f6
Alex Partin <25892264+adpartin@users.noreply.github.com>
2024-08-27 10:55:51 -0500

    delete params_cs.txt and params_ws.txt

--- conda_env_py37.sh
+++ conda_env_py37.sh
@@ -2,31 +2,39 @@
 
 set -e
 
-# Manually run these commands before running this sciprt
-# conda create -n GraphDRP_py37 python=3.7 pip --yes
-# conda activate GraphDRP_py37
+# Run this on Lambda
+# source /etc/profile.d/lmod.sh
+# which nvcc
+# module avail
+# module load cuda/11.8
+# which nvcc
+
+# CONDA_ENV_NAME=graphdrp_py37 # Conda env name
+# conda create -n $CONDA_ENV_NAME python=3.7 pip --yes # Create conda env
+# conda activate $CONDA_ENV_NAME # Activate conda env
 
 # conda install pytorch torchvision cudatoolkit=10.2 -c pytorch --yes
 # conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=10.2 -c pytorch
 conda install pytorch==1.12.1 torchvision==0.13.1 cudatoolkit=10.2 -c pytorch
 conda install pyg=2.1.0 -c pyg -c conda-forge --yes
 
-conda install -c conda-forge matplotlib --yes
-conda install -c conda-forge h5py=3.1 --yes
+conda install -c conda-forge matplotlib --yes # installed with candle
+conda install -c conda-forge h5py=3.1.0 --yes
 
-conda install -c bioconda pubchempy --yes
-conda install -c rdkit rdkit --yes
-conda install -c anaconda networkx --yes
-conda install -c conda-forge pyarrow=10.0 --yes
+conda install -c bioconda pubchempy=1.0.4 --yes
+conda install -c rdkit rdkit=2020.09.1.0 --yes # also installs pandas
+conda install -c anaconda networkx=2.6.3 --yes
+# conda install -c conda-forge pyarrow=10.0 --yes
+# conda install -c conda-forge pyarrow=8.0.0 --yes
+conda install conda-forge::pyarrow  # potential issues!?
 
 conda install -c pyston psutil --yes
 
-# IMPROVE
-pip install git+https://github.com/ECP-CANDLE/candle_lib@develop # CANDLE
+# Install CANDLE
+pip install git+https://github.com/ECP-CANDLE/candle_lib@develop
 
-# Other
+# # Other
 # conda install -c conda-forge ipdb=0.13.9 --yes
-# conda install -c conda-forge jupyterlab=3.2.0 --yes
 # conda install -c conda-forge python-lsp-server=1.2.4 --yes
 
 # Check installs
